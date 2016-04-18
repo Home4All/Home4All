4dfb6e6cf3234090eba2a442faf0a7fd0e368945
patelanuj21 <patel.anuj21@gmail.com>
2016-04-17 20:10:54 -0700

    Initial Commit

---
+++ Home4All/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Home4All
+//
+//  Created by Anuj Patel on 4/17/16.
+//  Copyright © 2016 Home4All. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
