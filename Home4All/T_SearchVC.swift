5a373f0543edfc674840e7e4417947af7557c838
ashish816 <ashish88.it@gmail.com>
2016-05-12 17:05:34 -0700

    Save Search.

--- Home4All/T_SearchVC.swift
+++ Home4All/T_SearchVC.swift
@@ -75,6 +75,14 @@ class T_SearchVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         self.locationManager.startUpdatingLocation()
 }
     
+    override func viewDidAppear(animated: Bool) {
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
